390e539ec28f1dc5a800a0e8c3ea246dd5c69d57
Benjamin Cook <bengcook55@gmail.com>
2022-11-16 22:28:48 -0500

    commit tree 25be0386578058ee841b90e346a7c4fa30c13909

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(names, string) {
+    const result = names.filter(drivers => string.toLowerCase() === drivers.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(initial, strings) {
+    const beforeEach = initial.filter(drivers => drivers.slice(0,2) === strings);
+    return beforeEach;
+}
+
+function matchName(driversMatch, name) {
+    return driversMatch.filter((driver) => driver.name === name);
+}
